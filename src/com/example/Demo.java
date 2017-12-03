42b065a2ce4ce5b6c2d736cfa1c9f216d683f35b
KalashM <33932773+KalashM@users.noreply.github.com>
2017-12-03 19:40:25 +0200

    Task7 modofoed to be used in Demo class

--- src/com/example/Demo.java
+++ src/com/example/Demo.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Demo {
   //hello Maryna
   public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class Demo {
       int sumFact = Task4.sumOfFactSequence(8);
       int luckyNum = Task5.numberOfLuckyNumbers(100001,100100);
       int[] testFibonachi = Task6.arrayOfFibonachi(20);
-
+      int[] arrSimpleNums = Task7.getListOfSimpleNumbers(7);
+      char[][] arrChess = Task8.chessArray(5,5);
 
       System.out.println("maxDiv = " + maxDiv);
       System.out.println("sum = " + sum);
@@ -20,6 +23,10 @@ public class Demo {
       for (int i=0; i<testFibonachi.length; i++) {
           System.out.println(testFibonachi[i]);
       }
+      for (int i=0; i<arrSimpleNums.length; i++) {
+          System.out.println(arrSimpleNums[i]);
+      }
+      System.out.println(Arrays.deepToString(arrChess));
   }
 
 }
--- src/com/example/Task7.java
+++ src/com/example/Task7.java
@@ -3,32 +3,21 @@ package com.example;
 import java.util.ArrayList;
 
 public class Task7 {
-    public static void main(String[] args) {
-        int[] arrRes = new int[100];
-
-
-
-        for (int j = 0; j < arrRes.length; j++) {
+    public static int[] getListOfSimpleNumbers(int len) {
+        int[] arrRes = new int[len];
+        for (int j = 0; j < len; j++) {
             arrRes[j] = 0;
-            //for (int i = j+1; arrRes[j] == 0 ; i++) {
             int i;
             for (i = j + 1;  ; i++) {
-                while (arrRes[j] == 0) {
-
-                    if (Task3.checkToSimple(i)) {
-                        arrRes[j] = i;
-                    } else {
-                        arrRes[j] = 0;
-                    }
-                    break;
+                if (Task3.checkToSimple(i)) {
+                    arrRes[j] = i;
+                } else {
+                    arrRes[j] = 0;
                 }
                 break;
             }
-            System.out.println(arrRes[j]);
+
         }
+        return arrRes;
     }
-    /*public static ArrayList getListOfSimpleNumbers (int len) {
-
-
-    }*/
 }
